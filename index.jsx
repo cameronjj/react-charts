bd26e3b5785cc42891d2b7025842d658e3610a37
Tom Yeh <doubleshow@gmail.com>
2016-08-16 05:05:28 -0600

    create Viz component

--- index.jsx
+++ index.jsx
@@ -1,16 +1,12 @@
-require("./node_modules/bootstrap/dist/css/bootstrap.min.css")
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import Viz from './viz'
 export class App extends React.Component {
 	render() {
 		return (
-			<div>
-				<h1>Jumbotron</h1>
-				<p>This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
-				<p><a className="btn btn-primary btn-lg">Learn more</a></p>
-			</div>
-		);
+			<Viz/>
+		)
 	}
 }
 
