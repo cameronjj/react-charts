372584b7deced8570a1af0fc8c00e23d96e35202
Tom Yeh <doubleshow@gmail.com>
2016-08-16 10:44:53 -0600

    create viz1 and viz2 routes placeholders

--- index.jsx
+++ index.jsx
@@ -2,18 +2,35 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import TinyBarChart from './chart'
 import TinyBarChartFoo from './chart2'
+import { Router, Route, Link, browserHistory } from 'react-router'
+
+// <div>
+//   <h1>Jumbotron</h1>
+//   <p>This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+//   <p><a className="btn btn-primary btn-lg">Learn more</a></p>
+//   <TinyBarChart height={200} dataKey={'uv'}/>
+//   <TinyBarChartFoo height={200} dataKey={'uv'}/>
+// </div>
+// );
+
+class Viz1 extends React.Component{
+  render(){
+    return <div>Hi Viz 1</div>
+  }
+}
+
+class Viz2 extends React.Component{
+  render(){
+    return <div>Hi Viz 2</div>
+  }
+}
 
 export class App extends React.Component {
 	render() {
-		return (
-			<div>
-				<h1>Jumbotron</h1>
-				<p>This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
-				<p><a className="btn btn-primary btn-lg">Learn more</a></p>
-        <TinyBarChart height={200} dataKey={'uv'}/>
-        <TinyBarChartFoo height={200} dataKey={'uv'}/>
-			</div>
-		);
+		return (<Router history={browserHistory}>
+    <Route path="viz1" component={Viz1}/>
+    <Route path="viz2" component={Viz2}/>
+  </Router>)
 	}
 }
 
