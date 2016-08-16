aa226749f218a2375db32d6b63fb88de293d6011
Tom Yeh <doubleshow@gmail.com>
2016-08-16 10:34:46 -0600

    move chart to a separte file

--- index.jsx
+++ index.jsx
@@ -1,31 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {BarChart, Bar, LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend} from 'recharts'
-
-const data = [
-      {name: 'Page A', uv: 4000, pv: 2400, amt: 2400},
-      {name: 'Page B', uv: 3000, pv: 1398, amt: 2210},
-      {name: 'Page C', uv: 2000, pv: 9800, amt: 2290},
-      {name: 'Page D', uv: 2780, pv: 3908, amt: 2000},
-      {name: 'Page E', uv: 1890, pv: 4800, amt: 2181},
-      {name: 'Page F', uv: 2390, pv: 3800, amt: 2500},
-      {name: 'Page G', uv: 3490, pv: 4300, amt: 2100},
-];
-
-class TinyBarChart extends React.Component{
-	render () {
-		const height = this.props.height
-		const dataKey = this.props.dataKey
-  	return (
-			<div>
-				<h1> {dataKey} vs testing </h1>
-    	<BarChart width={150} height={height} data={data}>
-         <Bar dataKey={dataKey} fill='#8884d8'/>
-       </BarChart>
-		 </div>
-    );
-  }
-}
+import TinyBarChart from './chart'
 
 export class App extends React.Component {
 	render() {
@@ -34,7 +9,8 @@ export class App extends React.Component {
 				<h1>Jumbotron</h1>
 				<p>This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
 				<p><a className="btn btn-primary btn-lg">Learn more</a></p>
-        <TinyBarChart height={500} dataKey={'uv'}/>
+        <TinyBarChart height={200} dataKey={'uv'}/>
+        <TinyBarChart height={200} dataKey={'uv'}/>
 			</div>
 		);
 	}
